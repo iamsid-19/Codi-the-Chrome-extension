4fd185b5c5953fdd66771c9b767681fd8b76c187
siddharth mishra <sid1402mishra@gmail.com>
2025-08-08 20:10:02 +0530

    added timer in the codi

---
+++ popup.js
@@ -0,0 +1,84 @@
+const numOfQues = document.getElementById('totalQues')
+const timer = document.querySelector('#timer')
+const startBtn = document.querySelector('#start')
+const countdown = document.querySelector('#countdown')
+const stopBtn = document.querySelector('#stop')
+const welcomeMsg = document.querySelector('#welcomeMsg')
+const timerDisplay =document.querySelector('.timer-display')
+let isChallengeStart = false;
+let timerInterval;
+
+
+function handlingTimer(timerInSec) {
+
+  if (timerInterval) {
+    clearInterval(timerInterval)
+  }
+  let currentTimerInSec = timerInSec;
+  const updateTimer = () => {
+    const hours = Math.floor(currentTimerInSec / 3600);
+    const mins = Math.floor((currentTimerInSec % 3600) / 60);
+    const secs = currentTimerInSec % 60;
+    const formattedTime = String(hours).padStart(2, '0') + ':' + String(mins).padStart(2, '0') + ':' + String(secs).padStart(2, '0');
+    countdown.textContent = formattedTime
+
+    chrome.storage.local.set({
+      timerInSec: currentTimerInSec
+    })
+    if (currentTimerInSec <= 0) {
+      clearInterval(timerInterval)
+      chrome.storage.local.remove('timerInSec');
+      countdown.textContent = "00:00:00";
+      alert("Time's up !")
+      return;
+    } else {
+      currentTimerInSec--;
+    }
+  }
+  updateTimer(); // Call the function immediately to show the initial time
+  timerInterval = setInterval(updateTimer, 1000)
+
+}
+startBtn.addEventListener('click', () => {
+  const checkboxes = document.querySelectorAll('input[name="difficulty"]:checked')
+  const selected = [];
+  if (!isChallengeStart) {
+    isChallengeStart = true;
+    welcomeMsg.style.display = 'none';
+    timerDisplay.style.display = 'block'
+    stopBtn.style.display='block'
+  }
+  else {
+    isChallengeRunning = false;
+    welcomeMsg.style.display = 'block';
+    timerDisplay.style.display = 'none';
+     stopBtn.style.display='none'
+  }
+  checkboxes.forEach(checkbox => selected.push(checkbox.value))
+  const questions = numOfQues.value;
+
+  //timer logic below
+  const timerValue = timer.value;
+  const timerInHrs = parseInt(timerValue, 10)
+  if (isNaN(timerInHrs) || timerInHrs === 0) {
+    alert("please select the timer")
+    return;
+  }
+  let timerInSec = timerValue * 3600;
+  handlingTimer(timerInSec)
+
+
+})
+document.addEventListener('DOMContentLoaded', () => {
+  chrome.storage.local.get(['timerInSec'], (data) => {
+    if (data.timerInSec) {
+      handlingTimer(data.timerInSec)
+
+    }
+  })
+})
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerInterval);
+  chrome.storage.local.remove('timerInSec')
+  countdown.textContent = "00:00:00";
+})
